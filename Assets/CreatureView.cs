2ff02ca2bb08e29559882114ef4526c81859adce
Trystan Spangler <trystan.s@comcast.net>
2016-03-06 05:58:56 -0800

    initial game, creatures, creature view

---
+++ Assets/CreatureView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class CreatureView : MonoBehaviour {
+	public SpriteRenderer CreatureSprite;
+
+	public Creature Creature;
+
+	void Start() {
+	
+	}
+	
+	void Update() {
+	
+	}
+
+	public void Initialize(Creature creature) {
+		Creature = creature;
+
+		var parts = creature.SpriteName.Split(':');
+		var sprites = Resources.LoadAll<Sprite>(parts[0]);
+		CreatureSprite.sprite = sprites.Single(s => s.name == parts[1]);
+
+		transform.position = new Vector3(creature.Position.X, creature.Position.Y, 0);
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class GameController : MonoBehaviour {
+	public GameObject CreaturePrefab;
+
+	Game game;
+	List<CreatureView> Views = new List<CreatureView>();
+
+	void Start() {
+		game = new Game();
+		game.Creatures.Add(new Creature() {
+			Position = new Point(1,1),
+			SpriteName = "DawnLike/Characters/Player0:Player0_1",
+		});
+
+		game.Creatures.Add(new Creature() {
+			Position = new Point(4,2),
+			SpriteName = "DawnLike/Characters/Player0:Player0_6",
+		});
+
+		foreach (var c in game.Creatures) {
+			var view = Instantiate(CreaturePrefab);
+			view.GetComponent<CreatureView>().Initialize(c);
+			Views.Add(view.GetComponent<CreatureView>());
+		}
+	}
+
+	void Update() {
+		
+	}
+}
+
+public struct Point {
+	public int X;
+	public int Y;
+
+	public Point(int x, int y) {
+		X = x;
+		Y = y;
+	}
+}
+
+public class Creature {
+	public Point Position;
+	public string SpriteName;
+}
+
+public class Game {
+	public List<Creature> Creatures = new List<Creature>();
+}
