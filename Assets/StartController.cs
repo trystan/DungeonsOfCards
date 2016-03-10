bcc7c12dc54f317663257279867872fa3564766f
Trystan Spangler <trystan.s@comcast.net>
2016-03-09 17:43:36 -0800

    add start screen

---
+++ Assets/StartController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartController : MonoBehaviour {
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return))
+			UnityEngine.SceneManagement.SceneManager.LoadScene(1, UnityEngine.SceneManagement.LoadSceneMode.Single);	
+	}
+}
